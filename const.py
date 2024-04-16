38bfab0ca1047dcfbbe5475a48be833fb7b2be22
Hanson <114070830+Hanson5732@users.noreply.github.com>
2024-04-16 23:50:27 +0800

    Update const.py

--- const.py
+++ const.py
@@ -5,9 +5,11 @@ GAME_HEIGHT_SIZE = 600
 
 START_COVER = "pic/start_cover.png"
 BACKGROUND_IMAGE = "pic/background.png"
+START_BUTTON_IMAGE = "pic/start.png"
 PAUSE_BUTTON_IMAGE = "pic/pause.png"
 RESTART_BUTTON_IMAGE = "pic/restart.png"
 BACKSTART_BUTTON_IMAGE = "pic/return.png"
+
 
 # Number the color to create block in random color
 class BlockType:
